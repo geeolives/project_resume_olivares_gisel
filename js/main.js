25be30bb4d6171fe56a72cd4bea6c7b95b914f61
Gisel Olivares <giselpolivares@icloud.com>
2017-04-07 02:55:35 +0200

    added more detailed comments to js file

--- js/main.js
+++ js/main.js
@@ -1,21 +1,25 @@
-// jQuery code to add hover to linked icons
+// jQuery code to add hover to icons with links
 $(document).ready(function () {
-
+// this part adds a hover effect to the email icon when scrolled over
     $('.email').hover(function () {
         $(this).attr('src', 'img/hoveremail.png');
     }, function () {
         $(this).attr('src', 'img/email.png');
     });
 });
-
+// this part adds a hover effect to the linkedin icon when scrolled over
 $('.linkedin').hover(function () {
-		$(this).attr('src', 'img/linkedinhover.png');
+    $(this).attr('src', 'img/linkedinhover.png');
 }, function () {
-		$(this).attr('src', 'img/linkedin.png');
+    $(this).attr('src', 'img/linkedin.png');
+});
+// this part adds a hover effect to the tumblr icon when scrolled over
+$('.tumblr').hover(function () {
+    $(this).attr('src', 'img/tumblrhover.png');
+}, function () {
+    $(this).attr('src', 'img/tumblr.png');
 });
 
-$('.tumblr').hover(function () {
-		$(this).attr('src', 'img/tumblrhover.png');
-}, function () {
-		$(this).attr('src', 'img/tumblr.png');
-});
+// first jQuery plugin
+
+// second jQuery plugin
